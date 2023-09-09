d66f9555a1a01bb300742e7b25bb35a7bba219fb
Asmit-Nepal <nasmit86@gmail.com>
2023-09-09 21:02:44 +0545

    views 1st commit

--- home/views.py
+++ home/views.py
@@ -1,21 +1,28 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 def home(request):
-
-    return render(request,'index.html')
+    views = {}
+    views['services'] = Services.objects.all()
+    views['feedbacks'] = Feedback.objects.all()
+    return render(request,'index.html',views)
 
 def about(request):
-        return render(request,'about.html')
+    return render(request,'about.html')
 
 def contact(request):
+
     return render(request,'contact.html')
 
 def portfolio(request):
+
     return render(request,'portfolio.html')
 
 def price(request):
+
     return render(request,'price.html')
 
 def services(request):
-    return render(request,'services.html')
+    views = {}
+    views['services'] = Services.objects.all()
+    return render(request,'services.html',views)
