8b96026e90b7c01678068dfaad9d98c2f2dfc60e
Asmit-Nepal <nasmit86@gmail.com>
2023-09-11 21:31:23 +0545

    Form POST method 1st Commit

--- home/views.py
+++ home/views.py
@@ -11,8 +11,23 @@ def about(request):
     return render(request,'about.html')
 
 def contact(request):
-
-    return render(request,'contact.html')
+    views = {}
+    views['contact_info'] =SiteInfo.objects.all()
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        subject = request.POST['subject']
+        message = request.POST['message']
+        data = Contact.objects.create(
+            name = name,
+            email = email,
+            subject = subject,
+            message = message
+        )
+        data.save()
+        views['success_messages'] = "The form has been submitted"
+        return render(request,'contact.html',views)
+    return render(request,'contact.html',views)
 
 def portfolio(request):
 
